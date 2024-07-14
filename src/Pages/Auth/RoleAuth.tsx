f8064c6d2d5226ab43f5ae02120703dae12bd815
ATHER-101 <atharvatijare04@gmail.com>
2024-07-14 17:14:42 +0530

    svh

--- src/Pages/Auth/RoleAuth.tsx
+++ src/Pages/Auth/RoleAuth.tsx
@@ -18,7 +18,7 @@ const RoleAuth = ({ role }: { role: string }) => {
   return (
     <Box
       sx={{
-        height: "100vh",
+        height: "100svh",
         width: "100vw",
         background: `linear-gradient(rgba(255, 87, 51, 0.2), rgba(255, 87, 51, 1)), url(https://images.unsplash.com/photo-1700308234428-c619d7408fbd?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`,
         backgroundRepeat: "no-repeat",
